326c8fb18a5966ad5de2cbf1681a5a9f085fc7e5
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:37:03 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let operacao = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    operacao = randint(0, 3)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
